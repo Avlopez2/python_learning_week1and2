e481715168b82506c35ec2f07934491ac8e4f594
Lopez <avlopez2@cps.k12.il.us>
2024-09-03 12:29:00 -0500

    first commit

---
+++ lesson.py
@@ -0,0 +1,28 @@
+# this is a print statement
+#print statements allow us to print out statements 
+# comments are used to help us understand the code 
+# print("hello my name is Ashley")
+# print("I am a student at John Hancock College Prep")
+# print("I am learning python")
+#create input for user
+name=input ("what is your name")
+print ("Hello",name)
+#input allows us to take input from the user
+
+
+#ask the user for their age 
+#print out the age
+#ask the user for their favorite color 
+#print out their favorite color
+#ask the user for their favorite food
+#print out the favorite food
+age=input ("How old are you?")
+print("I am", age )
+food=input ("what is your favorite food?")
+print("My favorite food is", food )
+#concatneation i sthe process of combining strign with
+#variables
+#print ("Your age", age, "years old")
+favorite_animal = input (" what is youre favorite animal?")
+print("your favorite animals is a " + favorite_animal)
+#the plus sign is used to concatneation
